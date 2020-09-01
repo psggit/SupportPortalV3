45d2055cf27b50fbe33052a7a9578bb1217faad1
hl05 <harshit@hipbar.com>
2020-09-02 01:37:36 +0530

    setting it up

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:14.7.0-stretch
+COPY . .
+
+ARG SENTRY_ENV=dev
+ARG SENTRY_RELEASE=default
+ARG BUILD_ENV=default
+ARG BASE_DOMAIN=default
+
+RUN $(npm bin)/webpack --mode production \
+  --env.SENTRY_ENV=${SENTRY_ENV} \
+  --env.SENTRY_RELEASE=${SENTRY_RELEASE} \
+  --env.BUILD_ENV=${BUILD_ENV} \
+  --env.BASE_DOMAIN=${BASE_DOMAIN}
+
+EXPOSE 8080
+ENTRYPOINT ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const pinoHttp = require("pino-http");
+const pino = require("pino");
+var serveStatic = require("serve-static");
+
+//Setup logger
+const httpLogger = pinoHttp();
+const logger = pino();
+
+const app = express();
+
+app.use(httpLogger);
+
+app.use(
+  serveStatic(path.join(__dirname, "dist/"), {
+    maxAge: "30d",
+  })
+);
+
+app.get("/*", (_, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(8080, () => {
+  logger.info({ port: 8080 }, "express started");
+});
