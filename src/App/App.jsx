c781cd711f39317290dccc15d693ab65720fdcd0
aparna <aparna.chinnaiah@hipbar.com>
2020-09-26 18:45:43 +0530

    Added basic structure and UI for DA

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { LoginContainer } from "./containers/Login/LoginContainer";
+import { LoginContainer } from "./containers/Login";
 import { DashboardContainer } from "./containers/Dashboard/Dashboard";
+import { DeliveryAgentContainer } from "./containers/DeliveryAgentDetails";
 import { ThemeProvider } from "@material-ui/styles";
 import newTheme from "./sass/theme";
 
@@ -13,6 +14,7 @@ function App() {
         <Router>
           <Switch>
             <Route path="/dashboard" component={DashboardContainer} />
+            <Route path="/order-details/123" component={DeliveryAgentContainer} />
             <Route path="/" component={LoginContainer} />
           </Switch>
         </Router>
