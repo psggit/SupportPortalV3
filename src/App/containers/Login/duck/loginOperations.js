1afce1313e188cf411bcb7094df82f7751d746f9
PurvaSawant16123 <purva.sawant@hipbar.com>
2020-10-24 16:34:08 +0530

    Redirect URL

--- src/App/containers/Login/duck/loginOperations.js
+++ src/App/containers/Login/duck/loginOperations.js
@@ -27,6 +27,7 @@ const onError = (dispatch) => {
 
 const getRedirectURL = () => {
   let redirectURL = "http://localhost:8080/dashboard";
+  console.log(process.env.NODE_ENV);
   switch (process.env.NODE_ENV) {
     case "local":
       return (redirectURL =
